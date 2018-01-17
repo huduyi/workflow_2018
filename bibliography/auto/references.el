a1613a22aaf6365ecd6404f53c539bc695af3110
huduyi <huduyi@163.com>
2018-01-17 17:36:39 +0800

    trying to restart

---
+++ bibliography/auto/references.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "malkiel03_effic_market_hypot_its_critic"
+    "nicholson68_price_ratios_relat_to_inves_resul"
+    "RePEc:ris:jofitr:1464"
+    "francis1960price-earnings"
+    "abarbanell97_fundam_analy_futur_earnin_stock_prices"
+    "11_techn_analy"))
+ :bibtex)
+
